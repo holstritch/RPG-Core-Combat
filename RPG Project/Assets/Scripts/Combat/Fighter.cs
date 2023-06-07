ed8324686d84ceabbce1efc57e9311a28bdcd30e
Hollie <hollienicolapage@gmail.com>
2023-06-07 12:50:00 +0100

    Add Player Controller and target for attack
    Added Player Controller script, namespaces, altered raycast to recognise target

---
+++ RPG Project/Assets/Scripts/Combat/Fighter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Combat
+{
+    public class Fighter : MonoBehaviour
+    {
+        void Update()
+        {
+        
+        }
+
+        public void Attack(CombatTarget target)
+        {
+            print("attack");
+        }
+    }
+}
+
---
+++ RPG Project/Assets/Scripts/Control/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using RPG.Combat;
+using RPG.Movement;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private Camera _mainCamera;
+        void Update()
+        {
+            InteractWithCombat();
+            InteractWithMovement();
+        }
+
+        private void InteractWithCombat()
+        {
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+
+            foreach (RaycastHit hit in hits)
+            {
+                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+
+                if (target == null)
+                {
+                    continue;
+                }
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    GetComponent<Fighter>().Attack(target);
+                }
+            }
+        }
+        private void InteractWithMovement()
+        {
+            if (Input.GetMouseButton(0))
+            {
+                MoveToCursor();
+            }
+        }
+
+        private void MoveToCursor()
+        {
+            Ray ray = GetMouseRay();
+            RaycastHit hit;
+            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
+
+            if (hasHit == true)
+            {
+                GetComponent<Mover>().MoveTo(hit.point);
+
+            }
+        }
+
+        private Ray GetMouseRay()
+        {
+            return _mainCamera.ScreenPointToRay(Input.mousePosition);
+        }
+    }
+}
+
