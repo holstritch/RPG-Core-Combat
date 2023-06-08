74a4bfb31706bc8e574b043bab41b4f7abba1092
Hollie <hollienicolapage@gmail.com>
2023-06-08 15:14:55 +0100

    Add death anim
    Added death anim triggered when health reaches 0

--- RPG Project/Assets/Scripts/Combat/Health.cs
+++ RPG Project/Assets/Scripts/Combat/Health.cs
@@ -8,10 +8,24 @@ namespace RPG.Combat
     {
         public float health = 100f;
 
+        private bool isDead = false;
         public void TakeDamage(float damage)
         {
             health = Mathf.Max(health - damage, 0);
             print(health);
+
+            if (health == 0)
+            {
+                Die();
+            }
+        }
+
+        private void Die()
+        {
+            if (isDead) return;
+            
+            GetComponent<Animator>().SetTrigger("die");
+            isDead = true;
         }
     }
 }
