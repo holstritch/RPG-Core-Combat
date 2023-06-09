69b47e65db16f26c85970c6ae228ab7c2b6bbdf6
Hollie <hollienicolapage@gmail.com>
2023-06-09 12:24:06 +0100

    Add enemy AI chase distance
    Added AI controller and chase distance

---
+++ RPG Project/Assets/Scripts/Control/AIController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+namespace RPG.Control
+{
+    public class AIController : MonoBehaviour
+    {
+        
+        [SerializeField] private float chaseDistance = 5f;
+
+        private GameObject _player;
+        private void Start()
+        {
+            _player = GameObject.FindWithTag("Player");
+        }
+
+        private void Update()
+        {
+            if (DistanceToPlayer(_player) < chaseDistance)
+            {
+                print(gameObject.name + " should chase");
+            }
+                
+        }
+
+        private float DistanceToPlayer(GameObject player)
+        {
+            
+            return Vector3.Distance(player.transform.position, transform.position);
+        }
+    }
+}
+
